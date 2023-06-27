28309e18d0700aa8c04972bbd10a91dda661b4f1
starshipxyz <starshipxyz@163.com>
2023-06-27 11:17:33 +0800

    add env

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello " + name + "!"
+    envVar = os.environ.get('token')
+    return "Hello " + name + '-' + envVar  + "!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
     
